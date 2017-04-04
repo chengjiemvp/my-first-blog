af33c3556e838f8be9c9f0dc24301d30713dcb4d
chengjie <765925079@qq.com>
2017-04-04 13:23:12 +0800

    编写博客的主页

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
     '''个人博客的主页'''
-    return render(request,'blog/index.html')
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by(
+            '-published_date'
+            )
+    return render(request,'blog/index.html',{'posts':posts})
+
