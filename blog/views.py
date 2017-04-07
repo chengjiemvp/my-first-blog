010a085449d31f8abe7fe77ffef0fb852b1bbdd7
chengjie <765925079@qq.com>
2017-04-07 17:52:31 +0800

    text[:50]

--- blog/views.py
+++ blog/views.py
@@ -11,9 +11,9 @@ from .forms import PostForm
 @login_required
 def index(request):
     '''个人博客的主页，显示所有文章'''
-    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by(
-            '-published_date'
-            )
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    for post in posts:
+        post.text=post.text[:222]+'.........'
     return render(request,'blog/index.html',{'posts':posts})
 def new_article(request):
     '''发布新文章'''
